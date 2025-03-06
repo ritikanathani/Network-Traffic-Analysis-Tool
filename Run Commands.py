78a0ff3bc3485960d48c9bcf6e503a3bb824d268
ritika nathani <127687969+ritikanathani@users.noreply.github.com>
2025-03-06 14:20:47 -0500

    Create Run Commands.py

---
+++ Run Commands.py
@@ -0,0 +1,11 @@
+##Run the traffic analysis:
+python scripts/analyze_traffic.py
+
+
+
+##Run anomaly detection:
+python scripts/anomaly_detection.py
+
+
+##Simulate a DoS attack:
+python scripts/simulate_attack.py
